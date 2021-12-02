a201fbd696bf4ff48480c65f3c5c22476f26e2e5
МК <92248408+hsemmzykin@users.noreply.github.com>
2021-12-02 16:28:25 +0300

    main declared

---
+++ inheritance/main.cpp
@@ -0,0 +1,9 @@
+#include "parent.h"
+#include <iostream> 
+int main(){
+    /**
+    * Here we're demonstrating the self-made python-style variables. Basically, we have created a universal type (aka auto) wich converts any type to var. 
+    * Our project is intended to show how to use inheritance in C++.
+    **/ 
+
+}
